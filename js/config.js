4d9c62b3ca16dcca6d46aabc1a475a5c79b5ca23
SGBC <orangenote604@gmail.com>
2026-05-19 11:36:26 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_BASE_URL = 'REPLACE_WITH_YOUR_GAS_WEBAPP_URL';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxefnTfZUGES42I2fnP1DNBrJGslhb30yY6MgCnheHA9KZNzYIgPqqlNcfXkul_th5Q/exec';
