db6d616c063c5a0a9206635ae7a69d32bd6ae8cb
SGBC <orangenote604@gmail.com>
2026-05-19 13:48:34 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbyp-fQT89sCVeaOc-Dmysb0O4mgDQ2jFJVuKp8UAMK0yj3o_jXr9TgbzyqmmActpxYr/exec';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbwq-APGWyXPLP4gwgkB-252pkjJ9OBmyeJfOMr5GxtwkROtmSrh7_95l4TU_WIKbZmK/exec';
