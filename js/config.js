418530b6d8e59abf2245784dd4cc0710d9b2f58d
SGBC <orangenote604@gmail.com>
2026-08-31 12:18:41 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbwukgQry4L4PrwKZWlK4N-74kHE6aqYTYgbOyPpfm5gGyPQvQKUV07Nl0pg65gUzGBB/exec';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbw9IAj3nmBbEOHeZbDlkQ0_nXNqa7umlDVIqewbbwSwfM5TFn6fDAZKvvlhUOGJZf_N/exec';
