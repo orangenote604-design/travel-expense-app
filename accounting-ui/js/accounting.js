e9e5771c912a310feb108e4a35a7ccd5f4cec7d1
SGBC <orangenote604@gmail.com>
2026-09-04 15:29:01 +0900

    Update accounting.js

--- accounting-ui/js/accounting.js
+++ accounting-ui/js/accounting.js
@@ -1,7 +1,7 @@
 const APP_CONFIG = {
   // 既存旅費申請システムで使用しているGASの実行URLを設定してください
   // 例: https://script.google.com/macros/s/AKfycbxISn7y_PZR-VPSRbcL6qSnd11QtoaQD8losokcve_ix9m7IM9aRu5Ub5JDRJE4fNMi/exec
-  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbxISn7y_PZR-VPSRbcL6qSnd11QtoaQD8losokcve_ix9m7IM9aRu5Ub5JDRJE4fNMi/exec'
+  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbx2Ig7BPUW8WEv1W-dhnyEsop4I7TLd2Gi0e8p7_EbRzSb9dJAdBbicCEMgrb9DmGA/exec'
 };
 
 const storageKeys = {
